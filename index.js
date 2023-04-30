d8e416905294963533b3105f04de8c00aeaf6b64
ILYA <106765406+D15ND@users.noreply.github.com>
2023-05-01 01:25:51 +0300

    fix: change event.key to event.code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const keyboard=['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ShiftRight', 'Control', 'Alt', 'Meta', ' ', 'Meta', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp'];
+const keyboard=['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'];
+const visualkey=['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'delete', 'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\'', 'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'return', 'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'shift', 'control', 'option', 'command', ' ', 'command', 'option', '←', '↑', '↓', '→'];
 document.onkeydown=function(event) {
-  console.log(event);
+  console.log(event.code);
 }
 
 function init() {
@@ -16,11 +17,11 @@ function init() {
 init();
 
 document.onkeydown=function(event) {
-  document.querySelector('.key__btn[data="' + event.key +'"]').classList.add('active');
+  document.querySelector('.key__btn[data="' + event.code +'"]').classList.add('active');
 };
 
 document.onkeyup=function(event) {
-  document.querySelector('.key__btn[data="' + event.key +'"]').classList.remove('active');
+  document.querySelector('.key__btn[data="' + event.code +'"]').classList.remove('active');
 };
 
 document.querySelectorAll('.key__btn').forEach(function(element) {
