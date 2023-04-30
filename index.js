1f13b90b6089ee42c69e8bbcb11407faaa37a534
ILYA <106765406+D15ND@users.noreply.github.com>
2023-05-01 00:35:04 +0300

    fix: delete some strings

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const keyboard=['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ShiftRight', 'Control', 'Alt', 'Meta', ' ', 'Meta', 'Alt', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp'];
 document.onkeydown=function(event) {
   console.log(event);
-  console.log(keyboard.push(key));
 }
 
 function init() {
