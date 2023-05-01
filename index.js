ce60a6b24d6dd534c330816d5a6ed7434c34e2f4
ILYA <106765406+D15ND@users.noreply.github.com>
2023-05-01 11:43:01 +0300

    feat: add container to some buttons

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const keyboard=['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ShiftRight', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight', 'AltRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'];
-const visualkey=['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'delete', 'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\'', 'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'return', 'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', 'shift', 'control', 'option', 'command', ' ', 'command', 'option', '←', '↑', '↓', '→'];
+const visualkey=['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'delete', 'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '"', 'return', 'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '/', 'shift', 'control', 'option', 'command', ' ', 'command', 'option', '←', '↑', '↓', '→'];
 document.onkeydown=function(event) {
   console.log(event.code);
 }
 
 function init() {
   let out='';
-  for (let i=0; i<keyboard.length; i++) {
+  for (let i=0; i<visualkey.length; i++) {
     if (i==14 || i==28 || i==41 || i==53) {
       out+='<div class="clear__fix"></div>';
     }
-    out+='<div class="key__btn" data="'+ keyboard[i]+'">'+keyboard[i]+'</div>';
+    if (i==60) {
+      out+='<div class="container"><div class="key__btn" data="'+ keyboard[i]+'">'+visualkey[i]+'</div><div class="key__btn" data="'+ keyboard[i+1]+'">'+visualkey[i+1]+'</div></div>';
+      i++;
+      continue;
+    }
+    out+='<div class="key__btn" data="'+ keyboard[i]+'">'+visualkey[i]+'</div>';
   }
   document.body.innerHTML=out;
 }
